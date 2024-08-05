06d4f1eb34f93c2aeada8f5fe2b1a79c847d2257
Dorin <dorin@gameanalytics.com>
2024-08-05 15:08:11 +0300

    new repository for the refactor

---
+++ source/gameanalytics/Common.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <cinttypes>
+#include <array>
+#include <algorithm>
+#include <memory>
+#include <thread>
+#include <utility>
+#include <chrono>
+#include <filesystem>
