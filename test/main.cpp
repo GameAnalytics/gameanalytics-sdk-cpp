c4379648dc0a2d8154b2a609153ffcae3b0597ce
Andrei Dabija <andreid@gameanalytics.com>
2024-10-04 03:18:07 +0300

    Update main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -3,6 +3,13 @@
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	
+	    if (sizeof(void*) == 8) {
+        std::cout << "64-bit architecture" << std::endl;
+    } else if (sizeof(void*) == 4) {
+        std::cout << "32-bit architecture" << std::endl;
+    } else {
+        std::cout << "Unknown architecture" << std::endl;
+    }
 	// Run only a specific test, e.g., "GATests.testInitialize"
 //	::testing::GTEST_FLAG(filter) = "GATests.testInitialize";
 	
